f4e8d529f133889beb40b5b333a00a02aeba61b1
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 17:37:36 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -2,17 +2,10 @@
 
 # shellcheck disable=SC2086
 
-set -e
-
-if [ -n "${BASH_VERSION:-}" ]; then
-    # shellcheck disable=SC3044
-    shopt -s expand_aliases
-fi
-
-alias trace_on='set -x'
-alias trace_off='{ set +x; } 2>/dev/null'
-
 dir=$(dirname "$(readlink -f "$0")")
+# shellcheck source=/dev/null
+. "$dir/cbase/common.sh"
+
 program=$(basename "$(readlink -f "$(dirname "$0")")")
 program_path="bin/$program"
 library_path="bin/$program.so"
