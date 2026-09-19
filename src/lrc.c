cff381a1fa8a71adf3a10e7080041bc6c3f6adf2
lucas-mior <lucas.mior@tutamail.com>
2026-09-19 18:14:00 -0300

    wip

--- src/lrc.c
+++ src/lrc.c
@@ -1391,7 +1391,7 @@ lrc_test_write_generated_file(void) {
         fatal(lrc_test_fail("read generated lrc file"));
     }
     ASSERT(text_len == strlen32(expected));
-    ASSERT_EQUAL(text, &expected[0]);
+    ASSERT_EQUAL(text, expected);
 
     free2(text, ((int64)text_len + 1)*SIZEOF(*text));
     test_remove_tree(temp_dir);
