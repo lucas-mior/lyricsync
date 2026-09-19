14c36b9a6eeadbff3dff142d1caffc7835e8b0d3
lucas-mior <lucas.mior@tutamail.com>
2026-09-19 18:00:03 -0300

    wip

--- cbase/memory.c
+++ cbase/memory.c
@@ -649,8 +649,10 @@ free_debug(char *file, int32 line, char *func,
 
         if (info.reallocated == -1) {
             ASSERT(MEMORY_CHECK_DOUBLE_FREE);
-            error_impl(file, line, func, "Double free.\n");
-            error_impl(info.file, info.line, info.func, "Freed here.\n");
+            error_impl(file, line, func,
+                       "Double free (size = %lld).\n", size);
+            error_impl(info.file, info.line, info.func,
+                       "Freed here. (size = %lld).\n", info.size);
             fatal(EXIT_FAILURE);
         }
         if (info.size != size) {
